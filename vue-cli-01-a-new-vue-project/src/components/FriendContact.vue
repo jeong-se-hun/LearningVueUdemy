bb76327dfc67954edfc65ded72a738d3f60cb27c
jeong-se-hun <88763715+jeong-se-hun@users.noreply.github.com>
2024-01-31 13:48:37 +0900

    섹션7: Vue CLI 개발 설정

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+    </h2>
+    <button @click="onClickDetailButton">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone: {{ friend.phone }}</strong>
+      </li>
+      <li>
+        <strong>email: {{ friend.email }}</strong>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    friend: {
+      id: String,
+      name: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    onClickDetailButton() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
